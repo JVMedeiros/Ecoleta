8319a430e63dc61f9746d976ac818fa99e89fa6f
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-23 11:21:14 -0300

    Routes usage setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,47 +1,12 @@
-import express, { response } from 'express';
-import { request } from 'http';
+import express from 'express';
+
+//Routes
+import routes from './routes';
+
 
 const app = express();
 
 app.use(express.json());
 
-
-const users = [
-    'João',
-    'Batman',
-    'Flash',
-    'Rufos'
-];
-
-app.get('/users', (request, response) => {
-    const search = String(request.query.search);
-
-    const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
-
-    return response.json(filteredUsers);
-});
-
-app.get('/users/:id', (request, response) => {
-    
-    const id = Number(request.params.id);
-
-    const user = users[id];
-
-    return response.json(user);
-});
-
-app.post('/users', (request, response ) => {
-    const data = request.body;
-
-    console.log(data);
-
-
-    const user = {
-        name: data.name,
-        email: data.email,
-    };
-    
-    return response.json(user);
-});
 
 app.listen(3333);
