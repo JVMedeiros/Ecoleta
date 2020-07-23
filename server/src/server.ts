989074ce643be79f16bdbda54cead340c36806f1
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-23 19:41:28 -0300

    Conflict resolve

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,6 +9,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads'));)
+app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')));
 
 app.listen(3333);
