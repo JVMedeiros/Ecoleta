83dbceab33b9b90a475a00cc7cb1efafc785eaf3
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-23 07:26:27 -0300

    Unique user .get route created

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,37 @@
 import express, { response } from 'express';
+import { request } from 'http';
 
 const app = express();
+
+const users = [
+    'João',
+    'Batman',
+    'Flash',
+    'Rufos'
+];
 
 app.get('/users', (request, response) => {
     console.log('Listagem de usuários');
 
-    response.json([
-        'João',
-        'Batman',
-        'Flash',
-        'Rufos'
-    ]);
+    return response.json(users);
+});
+
+app.get('/users/:id', (request, response) => {
+    
+    const id = Number(request.params.id);
+
+    const user = users[id];
+
+    return response.json(user);
+});
+
+app.post('/users', (request, response ) => {
+    const user = {
+        name: 'Diego',
+        email: 'diego@rocketseat.com.br',
+    };
+    
+    return response.json(user);
 });
 
 app.listen(3333);
