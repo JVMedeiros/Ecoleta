3b467274c9b13482a391ee99cc2ea04d74fd0cde
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-26 07:06:48 -0300

    update

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -22,6 +22,73 @@ const CreatePoint = () => {
                     Voltar para página inicial
                 </Link>
             </header>
+
+            <form>
+                <h1>Cadastro do <br/> ponto de coelta</h1>
+                {/* Collection point info containter*/}
+                <fieldset>
+                    <legend>
+                        <h2>Dados</h2>
+                    </legend>
+                    <div className="field">
+                        <label htmlFor="name">Nome da entidade</label>
+                        <input 
+                        type="text"
+                        name="name"
+                        id="name"
+                        />
+                    </div>
+                    <div className="field-group">
+                    <div className="field">
+                        <label htmlFor="email">E-mail</label>
+                        <input 
+                        type="email"
+                        name="email"
+                        id="email"
+                        />
+                    </div>
+                    <div className="field">
+                        <label htmlFor="whatsapp">WhatsApp</label>
+                        <input 
+                        type="text"
+                        name="whatsapp"
+                        id="whatsapp"
+                        /> 
+                    </div>
+                    </div>
+                </fieldset>
+                
+                {/* Location of Collection point container*/}
+                <fieldset>
+                    <legend>
+                        <h2>Endereço</h2>
+                        <span>Selecione o endereço no mapa</span>
+                    </legend>
+                    {/* City and State containter*/}
+                    <div className="field-group">
+                        <div className="field">
+                            <label htmlFor="uf">Estado (UF)</label>
+                            <select name="uf" id="uf">
+                                <option value="0">Selecione uma UF</option>
+                            </select>
+                        </div>
+
+                        <div className="field">
+                            <label htmlFor="city">Cidade</label>
+                            <select name="city" id="city">
+                                <option value="0">Selecione uma cidade</option>
+                            </select>
+                        </div>
+                    </div>
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        <h2>Itens de coleta</h2>
+                        <span>Selecione um ou mais itens de coleta</span>
+                    </legend>
+                </fieldset>
+            </form>
         </div>
     )
 };
