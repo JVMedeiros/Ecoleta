d1c95df572f106f666aa3882c85d53ab32c9dab1
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-26 04:56:07 -0300

    Code cleanup

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,21 +4,11 @@ import React, { useState } from 'react';
 import './App.css';
 
 //Components
-import Header from './Header';
+
 
 function App() {
-  const [counter, setCounter] = useState(0);
-
-  function handleButtonClick() {
-    setCounter(counter +1); 
-  }
-
   return (
-    <div>
-      <Header title="Ecoleta" />
-      <h1>{counter}</h1>
-      <button type="button" onClick={handleButtonClick}>Acrescentar</button>
-    </div>
+    <h1>Ecoleta</h1>
   );
 }
 
