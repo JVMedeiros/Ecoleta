04d786516f2f52083787eb911f84fd21d1484839
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-26 14:41:15 -0300

    sakurando

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
             return {
                 id: item.id,
                 title: item.title,
-                image_url: `${process.env.APP_URL}/uploads/${item.image}`,
+                image_url: `https://http://localhost:3333/uploads/${item.image}`,
             };
         });
     
