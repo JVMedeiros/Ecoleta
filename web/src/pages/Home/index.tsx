1b3a4ecece7e82570b12372f0dc690311dac1580
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-26 05:42:00 -0300

    Router manager done

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+//Router
+import { Link } from 'react-router-dom';
+
 //Icons
 import { FiLogIn } from 'react-icons/fi'
 
@@ -22,12 +25,12 @@ const Home = () => {
                     <h1>Seu marketplace de coleta de resíduos.</h1>
                     <p>Ajudamos pessoas a encontrarem pontos de coleta de forma eficiente.</p>
 
-                    <a href="/cadastro-ponto">
+                    <Link to="/cadastro-ponto">
                         <span>
                             <FiLogIn />
                         </span>
                         <strong>Cadastre um ponto de coleta</strong>
-                    </a>
+                    </Link>
                 </main>
             </div> 
         </div>
