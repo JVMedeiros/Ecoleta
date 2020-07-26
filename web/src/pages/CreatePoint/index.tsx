a3cc2cc46d7965163a69026cda62f6f23de73d24
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-07-26 05:46:21 -0300

    Header for create point page created

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+//Router
+import {Link} from 'react-router-dom';
+
+//Styles
+import 'styles.css';
+
+//Assets
+import logo from '../../assets/logo.svg'
+
 const CreatePoint = () => {
     return (
-        <h1>Ecoleta</h1>
+        <div id="page-create-point">
+            <header>
+                <img src={logo} alt="Ecoleta"/>
+            </header>
+        </div>
     )
 };
 
