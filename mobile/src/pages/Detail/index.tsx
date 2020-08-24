0c7d7b961318d0464b8ec61f08ba7d0e23585121
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-08-24 10:54:49 -0300

    cleanup

--- mobile/src/pages/Detail/index.tsx
+++ mobile/src/pages/Detail/index.tsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {View, StyleSheet, Text, Linking, Image, TouchableOpacity, SafeAreaView} from 'react-native';
-import {Feather as Icon, FontAwesome } from '@expo/vector-icons';
+import React, { useEffect, useState } from 'react';
+import { Feather as Icon, FontAwesome } from '@expo/vector-icons';
 import { useNavigation, useRoute } from '@react-navigation/native';
+import { View, StyleSheet, Text, Image, TouchableOpacity, SafeAreaView, Linking } from 'react-native';
 import { RectButton } from 'react-native-gesture-handler';
-import * as MailComposer from 'expo-mail-composer';
 import api from '../../services/api';
+import * as MailComposer from 'expo-mail-composer';
 
 interface Params {
   point_id: number;
@@ -31,25 +31,25 @@ const Detail = () => {
   const navigation = useNavigation();
   const route = useRoute();
 
-  const routeParms = route.params as Params;
+  const routeParams = route.params as Params;
+
+  useEffect(() => {
+    api.get(`points/${routeParams.point_id}`).then(response => {
+      setData(response.data);
+    });
+  }, []);
 
   function handleNavigateBack() {
     navigation.goBack();
   }
 
-  useEffect(() => {
-    api.get(`points/${routeParms.point_id}`).then(response => {
-      setData(response.data);
-    });
-  }, [])
-
   function handleWhatsapp() {
-    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Gostaria de utilizar o serviço de coleta de resíduos`)
+    Linking.openURL(`whatsapp://send?phone=${data.point.whatsapp}&text=Tenho interesse sobre coleta de resíduos`);
   }
 
   function handleComposeMail() {
     MailComposer.composeAsync({
-      subject: 'Coleta de resíduos',
+      subject: 'Interesse na coleta de resíduos',
       recipients: [data.point.email],
     })
   }
@@ -59,13 +59,14 @@ const Detail = () => {
   }
 
   return (
-    <SafeAreaView style={{flex: 1}}>
+    <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.container}>
-        <TouchableOpacity  onPress={handleNavigateBack}>
+        <TouchableOpacity onPress={handleNavigateBack}>
           <Icon name="arrow-left" size={20} color="#34cb79" />
         </TouchableOpacity>
 
-        <Image style={styles.pointImage} source={{ uri: data.point.image_url}}/>
+        <Image style={styles.pointImage} source={{ uri: data.point.image_url }} />
+      
         <Text style={styles.pointName}>{data.point.name}</Text>
         <Text style={styles.pointItems}>
           {data.items.map(item => item.title).join(', ')}
@@ -73,24 +74,24 @@ const Detail = () => {
 
         <View style={styles.address}>
           <Text style={styles.addressTitle}>Endereço</Text>
-          <Text style={styles.addressContent}>{data.point.uf}</Text>
+          <Text style={styles.addressContent}>{data.point.city}, {data.point.uf}</Text>
         </View>
+      </View>
+      <View style={styles.footer}>
+        <RectButton style={styles.button} onPress={handleWhatsapp}>
+          <FontAwesome name="whatsapp" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>Whatsapp</Text>
+        </RectButton>
 
-        <View style={styles.footer}>
-          <RectButton style={styles.button} onPress={() => {}}>
-            <FontAwesome name="whatsapp" size={20} color="#fff"/>
-            <Text style={styles.buttonText}>WhatsApp</Text>
-          </RectButton>
-
-          <RectButton style={styles.button} onPress={handleComposeMail}>
-            <Icon name="mail" size={20} color="#fff"/>
-            <Text style={styles.buttonText}>E-mail</Text>
-          </RectButton>
-        </View>
+        <RectButton style={styles.button} onPress={handleComposeMail}>
+          <Icon name="mail" size={20} color="#FFF" />
+          <Text style={styles.buttonText}>E-mail</Text>
+        </RectButton>
       </View>
     </SafeAreaView>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
