ab0957b4e9b4effb6a57abedcdde78b2b3c2e08c
JVMedeiros <jv.medeiros.gallina@gmail.com>
2020-08-25 13:20:18 -0300

    iamge uploadble

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,18 @@
 import express from 'express';
 
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 //Controller import
 import PointsController from './controllers/PointsController';
 import ItemsController from './controllers/ItemsController';
 
 //Routes Import
 const routes = express.Router();
+
+const upload = multer(multerConfig);
+
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
@@ -16,7 +23,7 @@ routes.post('/points', pointsController.create);
 //Route to show only one colect point
 routes.get('/points', pointsController.index);
 //Route to show only one colect point
-routes.get ('/points/:id', pointsController.show);
+routes.get ('/points/:id', upload.single('image'), pointsController.show);
 
 
 
